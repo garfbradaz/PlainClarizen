37357263457722e10a0ac9f9fdc96c2b40ef9811
Gareth Bradley <garfbradaz@outlook.com>
2015-06-26 15:24:55 +0100

    Removed JsonClassGenerator and added a little custom code to the Xamasoft DLL for adding inherited interfaces/classes.

--- PlainClarizen/PlainClarizen/Program.cs
+++ PlainClarizen/PlainClarizen/Program.cs
@@ -15,49 +15,55 @@ namespace PlainClarizen
     {
         static void Main(string[] args)
         {
-            RestClient client = new RestClient("garfbradaz", "Poohead26@");
+            //Add read line fpr namespace ;
+            var mainNameSpace = string.Empty;
+            var secondNameSpace = string.Empty;
+            var folder = string.Empty;
+
+            ReadInputFromUser(ref mainNameSpace, " Please enter a Main Namespace ");
+            ReadInputFromUser(ref secondNameSpace, " Please enter a Secondary Namespace ");
+
+            //TODO add logic to test this is a correct drive/folder.
+            ReadInputFromUser(ref folder, "Please enter the folder location to save your code files ");
+
+            ///need to change your password.
+            RestClient client = new RestClient("username", "password");
+
+            bool count = true;
+            int counting = 0;
             foreach(string s in client.ListEntities)
             {
+                counting++; 
                 client.GetAllMetadataDescribeEntitiesAndGetAllFields(s);
+                if(counting > 2 && count == true) break;
             }
+
 
 
             if (client.ConvertedMetadata != null)
+
             {
                 foreach (KeyValuePair<string, JToken> pair in client.ConvertedMetadata)
                 {
-                    var gen = Prepare(pair.Value, pair.Key);
-                    if (gen == null) return;
 
-                    try
-                    {
-                        gen.GenerateClasses();
+                        var gen = Prepare(pair.Value, pair.Key,folder,mainNameSpace,secondNameSpace);
+                        if (gen == null) return;
 
-                    }
-                    catch (Exception ex)
-                    {
-                        Console.Write("Error writing class " + ex + pair.Key + " " + pair.Value);
-                    }
+                        try
+                        {
+                            gen.GenerateClasses();
+
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.Write("Error writing class " + ex + pair.Key + " " + pair.Value);
+                        }
 
                 }
             }
-            /*
-            if (!string.IsNullOrWhiteSpace(client.ConvertedMetadata))
-            {
-                var gen = Prepare(client, entityType.Task);
-                if (gen == null) return;
 
-                try
-                {
-                    gen.GenerateClasses();
 
-                }
-                catch (Exception ex)
-                {
-                    Console.Write("Error writing class " + ex);
-                }
-            }
-             */
+            Console.Write("Finished! - code should be ready in folder " + folder + " ");
             Console.ReadLine();
         }
 
@@ -73,7 +79,7 @@ namespace PlainClarizen
 
             //Add read line fpr namespace ;
             var input = string.Empty;
-            ReadInputFromUser(ref input," Please enter a Namespace ");
+            //ReadInputFromUser(ref input," Please enter a Namespace ");
 
             gen.Namespace = "MainNameSpace.Main";
             gen.NoHelperClass = false;
@@ -98,7 +104,8 @@ namespace PlainClarizen
             gen.ExamplesInDocumentation = false;
             return gen;
         }
-        static JsonClassGenerator Prepare(JToken json, string type)
+        static JsonClassGenerator Prepare(JToken json, string type, string folder,
+            string mainNameSpace = "MainNameSpace.Main", string secondaryNameSpace = "SecondNameSpace.Main")
         {
 
             var gen = new JsonClassGenerator();
@@ -107,23 +114,24 @@ namespace PlainClarizen
             gen.CodeWriter = new CSharpCodeWriter();
             gen.ExplicitDeserialization = false;
 
-            //Add read line fpr namespace ;
-            var input = string.Empty;
-            ReadInputFromUser(ref input, " Please enter a Namespace ");
+            gen.Namespace = mainNameSpace;
+            gen.SecondaryNamespace = secondaryNameSpace;
 
-            gen.Namespace = "MainNameSpace.Main";
-            gen.NoHelperClass = false;
-            gen.SecondaryNamespace = "SecondNameSpace.Main";
+            //gen.Namespace = "MainNameSpace.Main";
+            //gen.SecondaryNamespace = "SecondNameSpace.Main";
             //gen.UseNamespaces = true;
-            gen.TargetFolder = @"D:\test data\PlainClarizen";
+            gen.NoHelperClass = false;
+
+            gen.TargetFolder = @folder;
             gen.UseProperties = true;
-            gen.MainClass = "Clarizen" + type;
+            gen.MainClass = type;
 
             gen.UsePascalCase = true;
             gen.UseNestedClasses = false;
             gen.ApplyObfuscationAttributes = false;
             gen.SingleFile = true;
             gen.ExamplesInDocumentation = false;
+            gen.InheritedClassOrInterface = "ClarizenEntity";
             return gen;
         }
 
