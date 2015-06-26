d6ecaf5d2131702576bba8fd5390f417c3293e70
garfbradaz <zimzum17_1998@hotmail.com>
2015-06-26 20:43:29 +0100

    Added more inputs

--- PlainClarizen/PlainClarizen/Program.cs
+++ PlainClarizen/PlainClarizen/Program.cs
@@ -8,6 +8,7 @@ using Xamasoft.JsonClassGenerator;
 using Xamasoft.JsonClassGenerator.CodeWriters;
 using System.Diagnostics;
 using Newtonsoft.Json.Linq;
+using System.Security;
 
 namespace PlainClarizen
 {
@@ -15,27 +16,31 @@ namespace PlainClarizen
     {
         static void Main(string[] args)
         {
-            //Add read line fpr namespace ;
             var mainNameSpace = string.Empty;
-            var secondNameSpace = string.Empty;
             var folder = string.Empty;
+            var userName = string.Empty;
+            var passWord = string.Empty;
 
+            ReadInputFromUser(ref userName, " Please enter your Clarizen Username");
+            ReadInputFromUserSecretly(ref passWord, "Please enter your Clarizen Password");
             ReadInputFromUser(ref mainNameSpace, " Please enter a Main Namespace ");
-            ReadInputFromUser(ref secondNameSpace, " Please enter a Secondary Namespace ");
 
             //TODO add logic to test this is a correct drive/folder.
             ReadInputFromUser(ref folder, "Please enter the folder location to save your code files ");
 
             ///need to change your password.
-            RestClient client = new RestClient("username", "password");
+            RestClient client = new RestClient(userName, passWord);
 
             bool count = true;
             int counting = 0;
-            foreach(string s in client.ListEntities)
+            if (client.ListEntities != null)
             {
-                counting++; 
-                client.GetAllMetadataDescribeEntitiesAndGetAllFields(s);
-                if(counting > 2 && count == true) break;
+                foreach (string s in client.ListEntities)
+                {
+                    counting++;
+                    client.GetAllMetadataDescribeEntitiesAndGetAllFields(s);
+                    //if (counting > 2 && count == true) break;
+                }
             }
 
 
@@ -46,7 +51,7 @@ namespace PlainClarizen
                 foreach (KeyValuePair<string, JToken> pair in client.ConvertedMetadata)
                 {
 
-                        var gen = Prepare(pair.Value, pair.Key,folder,mainNameSpace,secondNameSpace);
+                        var gen = Prepare(pair.Value, pair.Key,folder,mainNameSpace);
                         if (gen == null) return;
 
                         try
@@ -67,45 +72,8 @@ namespace PlainClarizen
             Console.ReadLine();
         }
 
-        static JsonClassGenerator Prepare(RestClient client,entityType type)
-         
-        {
-
-            var gen = new JsonClassGenerator();
-            gen.Example = client.ConvertedMetadata.ToString();
-            gen.InternalVisibility = false;
-            gen.CodeWriter = new CSharpCodeWriter();
-            gen.ExplicitDeserialization = false;
-
-            //Add read line fpr namespace ;
-            var input = string.Empty;
-            //ReadInputFromUser(ref input," Please enter a Namespace ");
-
-            gen.Namespace = "MainNameSpace.Main";
-            gen.NoHelperClass = false;
-            gen.SecondaryNamespace = "SecondNameSpace.Main";
-            //gen.UseNamespaces = true;
-            gen.TargetFolder = @"D:\test data\PlainClarizen";
-            gen.UseProperties = true;
-
-            switch(type)
-            {
-                case entityType.Task:
-                    gen.MainClass = "ClarizenTask";
-                        break;
-                default:
-                        break;
-            }
-            
-            gen.UsePascalCase = true;
-            gen.UseNestedClasses = false;
-            gen.ApplyObfuscationAttributes = false;
-            gen.SingleFile = true;
-            gen.ExamplesInDocumentation = false;
-            return gen;
-        }
         static JsonClassGenerator Prepare(JToken json, string type, string folder,
-            string mainNameSpace = "MainNameSpace.Main", string secondaryNameSpace = "SecondNameSpace.Main")
+            string mainNameSpace = "MainNameSpace.Main")
         {
 
             var gen = new JsonClassGenerator();
@@ -115,11 +83,6 @@ namespace PlainClarizen
             gen.ExplicitDeserialization = false;
 
             gen.Namespace = mainNameSpace;
-            gen.SecondaryNamespace = secondaryNameSpace;
-
-            //gen.Namespace = "MainNameSpace.Main";
-            //gen.SecondaryNamespace = "SecondNameSpace.Main";
-            //gen.UseNamespaces = true;
             gen.NoHelperClass = false;
 
             gen.TargetFolder = @folder;
@@ -190,6 +153,36 @@ namespace PlainClarizen
 
             }
 
+        }
+
+        static void ReadInputFromUserSecretly(ref string input, string displayMessage)
+        {
+            
+            Console.WriteLine(displayMessage);
+            ConsoleKeyInfo key;
+            // Backspace Should Not Work
+
+            do
+            {
+                key = Console.ReadKey(true);
+
+                if (key.Key != ConsoleKey.Backspace && key.Key != ConsoleKey.Enter)
+                {
+                    input += key.KeyChar;
+                    Console.Write("*");
+                }
+                else
+                {
+                    if (key.Key == ConsoleKey.Backspace && input.Length > 0)
+                    {
+                        input = input.Substring(0, (input.Length - 1));
+                        Console.Write("\b \b");
+                    }
+                }
+            }
+            while (key.Key != ConsoleKey.Enter);
+            Console.WriteLine();
+            
         }
     }
 }
